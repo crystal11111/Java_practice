d4e95156d25d6570ebc17105e76247d8cc8bf5af
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-17 10:52:59 +0900

    Create Ex1_first.java

---
+++ Day1/Ex1_first.java
@@ -0,0 +1,19 @@
+package ex01_first;
+
+public class Ex1_first {
+	
+	public static void main( String[] args ) {
+		//주석: 컴퓨터가 인지하지 못하는 개발자들간의 메모
+		//main method: 프로그램 실행시 가장 먼저 호출되는 영역
+		System.out.println(100);
+		System.out.println(10+10); 		//syso + ctrl + space bar 자동완성
+		System.out.println("안녕");
+		System.out.println("hello"+ 10);
+		System.out.println("홍"+ "길동");
+		System.out.println("1 + 1 = "+ 2);
+		System.out.println("77 + 24 = "+ 77+24); 	// 7724
+		System.out.println("77 + 24 = "+ (77+24)); 	// 101
+		System.out.println(1+1+"1+1"+1+1);
+	}
+}
+ 
