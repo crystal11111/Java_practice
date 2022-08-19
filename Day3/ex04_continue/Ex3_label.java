7919578823fafbe423efc7c3a9d4f81e6011e805
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-18 21:19:01 -0600

    Update Ex3_label.java

--- Day3/ex04_continue/Ex3_label.java
+++ Day3/ex04_continue/Ex3_label.java
@@ -1,9 +1,8 @@
-// label break
-out:for(int i=1; i<=5; i++){
+
+outer:for(int i=1; i<=3; i++){
   for(int j=1;j<=5; j++){
     if(j%2==0){
-      // out이라는 이름을 가진 반복문을 빠져나간다
-      break out;
+      continue outer;
     }
     syso(j+" ");
     
