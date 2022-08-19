3813313fd0179a09082f041556df55c73d2765d0
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-18 20:39:01 -0600

    Create Ex2_break.java

---
+++ Day3/ex03_break/Ex2_break.java
@@ -0,0 +1,19 @@
+package ex03_break;
+
+public class Ex2_break {
+	public static void main(String[] args) {
+    
+    int n =1;
+    
+    while (true){
+      n++;
+      syso(n);
+      
+      if(n>5){
+        break;
+      }
+    }//while
+    
+
+  }//main
+}
