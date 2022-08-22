b487ef1735d4a34310fdceb4256a0450e5c6ad4c
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-22 10:25:46 +0900

    Add files via upload

---
+++ Day3/ex05_work/Ex2_work.java
@@ -0,0 +1,28 @@
+package ex05_work;
+
+public class Ex2_work {
+	public static void main(String[] args) {
+		
+		//         *
+		//       * * *
+		//     * * * * *
+		//   * * * * * * *
+		// * * * * * * * * *
+		
+		for (int i =0; i<5; i++) {
+			for (int j = 0; j<5+i; j++) {
+				
+				if( i+j > 3) {
+					System.out.print("* ");
+				}else {
+					System.out.print("  ");
+				}
+				
+			}//inner
+			
+			System.out.println();
+		
+		}//outer
+		
+	}//main
+}
---
+++ Day3/ex05_work/Ex3_work.java
@@ -0,0 +1,8 @@
+package ex05_work;
+
+public class Ex3_work {
+
+	// 4는 소수가 아닙니다
+	
+	
+}
