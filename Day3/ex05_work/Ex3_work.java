f80b83387216f9886fbddfaf8a0e723abdc8cfae
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-22 10:37:50 +0900

    Update Ex3_work.java

--- Day3/ex05_work/Ex3_work.java
+++ Day3/ex05_work/Ex3_work.java
@@ -1,8 +1,27 @@
 package ex05_work;
 
 public class Ex3_work {
-
-	// 4는 소수가 아닙니다
+	public static void main(String[] args) {
+		// 4는 소수가 아닙니다
+		
+		int n = 3;
+		int i = 0;
+		
+		for (i=2; i<=n; i++) {
+			
+			if(n % i == 0) {
+				break;
+			}
+			
+		}//for
+		
+		if (i ==n) {
+			System.out.println(n + "은(는) 소수입니다");
+		}else {
+			System.out.println(n + "은(는) 소수가");
+		}
+		
 	
-	
+	}//main
+		
 }
