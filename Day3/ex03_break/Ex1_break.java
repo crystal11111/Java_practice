acb153f0550e8b453550c9416d63b6d9dfae8fd6
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-23 13:38:40 +0900

    Update Ex1_break.java

--- Day3/ex03_break/Ex1_break.java
+++ Day3/ex03_break/Ex1_break.java
@@ -2,23 +2,21 @@ package ex03_break;
 
 public class Ex1_break {
 	public static void main(String[] args) {
-      
-    // break문: 반복문 내에서 강제적으로 가장 가까운 반복문을 빠져나가기 위해 사용
-    
-    for (int i =1; i<=2; i++){
-      for (int j=1;j<=5;j++){
-         if(j%2==0){
-            break;
-         }
-            syso(j);
-        
-      }//inner
-      
-      syso();
-      
-    }//outer
-    
-    
-    
-  }//main
+		      
+	// break문: 반복문 내에서 강제적으로 가장 가까운 반복문을 빠져나가기 위해 사용
+			
+	for (int i =1; i<=2; i++){
+		for (int j=1;j<=5;j++){
+			if(j%2 == 0){
+		            break;
+			}
+			System.out.println(j);
+		        
+		}//inner
+		      
+		System.out.println();
+		      
+		}//outer
+		        
+	}//main
 }
