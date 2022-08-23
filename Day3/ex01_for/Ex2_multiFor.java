83fd585b9998fce45a2ac9b7c4d4cdee9e3e180d
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-23 13:34:31 +0900

    Update Ex2_multiFor.java

--- Day3/ex01_for/Ex2_multiFor.java
+++ Day3/ex01_for/Ex2_multiFor.java
@@ -1,12 +1,15 @@
+package ex01_for;
+
+public class Ex2_multiFor {
+	public static void main(String[] args) {
 
 
-int cnt =0;
-for(int i=1;i<=3;i++){
-  for(int j=1; j<=4;j++){
-    syso %02d, ++cnt
-    }
-  
-  for( int i=1; i<=5; i++){
-    for (int j =0 ; j<1; j++){
-      syso *
-    
+    int cnt =0;
+        for(int i=1; i<=3;i++){
+          for(int j=1; j<=4;j++){
+            System.out.print(%02d, ++cnt);
+            }
+        for(int i=1; i<=5; i++) {
+          for (int j =0 ; j<1; j++){
+            System.out.println("*");
+          }
