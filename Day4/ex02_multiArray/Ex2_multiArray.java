56f9e9248b2701743001ea5d562bd66047fa27a6
Crystal Cho <78241019+crystal11111@users.noreply.github.com>
2022-08-23 13:21:41 +0900

    Update Ex2_multiArray.java

--- Day4/ex02_multiArray/Ex2_multiArray.java
+++ Day4/ex02_multiArray/Ex2_multiArray.java
@@ -4,8 +4,8 @@ public class Ex2_multiArray {
 	public static void main(String[] args) {
 		
 		char[][] ch = {{'A', 'B'},
-					  {'C', 'D', 'E'},
-					  {'F'} };
+				{'C', 'D', 'E'},
+				{'F'} };
 		
 		for (int i= 0; i < ch.length;i++) {
 			for (int j=0; j < ch[i].length; j++) {
